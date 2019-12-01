98249449633a8bd584eb3e36b9b673288cd65c0c
Mattias Buelens <mattias@buelens.com>
2019-12-02 00:14:22 +0100

    Solve day 1 part 2

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = parse_input();
     part1(&input);
+    part2(&input);
 }
 
 fn parse_input() -> Vec<u32> {
@@ -13,11 +14,28 @@ fn parse_input() -> Vec<u32> {
 
 fn part1(masses: &Vec<u32>) {
     let total_fuel: u32 = masses.iter()
-        .map(|x| { get_fuel(*x) })
+        .map(|x| { get_fuel_part1(*x) })
         .sum();
     println!("Answer to part 1: {}", total_fuel);
 }
 
-fn get_fuel(mass: u32) -> u32 {
-    (mass / 3) - 2
+fn get_fuel_part1(mass: u32) -> u32 {
+    (mass / 3).saturating_sub(2)
+}
+
+fn part2(masses: &Vec<u32>) {
+    let total_fuel: u32 = masses.iter()
+        .map(|x| { get_fuel_part2(*x) })
+        .sum();
+    println!("Answer to part 2: {}", total_fuel);
+}
+
+fn get_fuel_part2(mut mass: u32) -> u32 {
+    let mut total_fuel = 0u32;
+    while mass > 0 {
+        let fuel = get_fuel_part1(mass);
+        total_fuel += fuel;
+        mass = fuel;
+    }
+    total_fuel
 }
