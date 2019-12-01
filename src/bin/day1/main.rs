c4633b9ea53c44f09a5c3ecd20f902abe7017a74
Mattias Buelens <mattias@buelens.com>
2019-12-02 00:05:33 +0100

    Solve day 1 part 1

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let input = parse_input();
+    part1(&input);
+}
+
+fn parse_input() -> Vec<u32> {
+    return include_str!("input")
+        .trim()
+        .split('\n')
+        .map(|x| { x.parse().expect("expected number") })
+        .collect();
+}
+
+fn part1(masses: &Vec<u32>) {
+    let total_fuel: u32 = masses.iter()
+        .map(|x| { get_fuel(*x) })
+        .sum();
+    println!("Answer to part 1: {}", total_fuel);
+}
+
+fn get_fuel(mass: u32) -> u32 {
+    (mass / 3) - 2
 }
