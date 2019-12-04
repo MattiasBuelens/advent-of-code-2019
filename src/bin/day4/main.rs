6a1c1a8e8a26c33a383a60edc24950d22687bc80
Mattias Buelens <mattias@buelens.com>
2019-12-04 13:13:56 +0100

    Solve day 4 part 2

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -4,10 +4,15 @@ fn main() {
     let start = 372037;
     let end = 905157;
     println!("Answer to part 1: {}", part1(start, end));
+    println!("Answer to part 2: {}", part2(start, end));
 }
 
 fn part1(start: u32, end: u32) -> i32 {
     solve(start, end, is_password_part1)
+}
+
+fn part2(start: u32, end: u32) -> i32 {
+    solve(start, end, is_password_part2)
 }
 
 fn solve<F: Fn([u8; 6]) -> bool>(start: u32, end: u32, is_password: F) -> i32 {
@@ -47,6 +52,29 @@ fn is_password_part1(digits: [u8; 6]) -> bool {
     has_same
 }
 
+fn is_password_part2(digits: [u8; 6]) -> bool {
+    let mut has_group_of_2_digits = false;
+    let mut current_group_count: u8 = 1;
+    for i in 1..digits.len() {
+        match digits[i - 1].cmp(&digits[i]) {
+            Ordering::Equal => {
+                current_group_count += 1;
+            }
+            Ordering::Less => {
+                if current_group_count == 2 {
+                    has_group_of_2_digits = true
+                }
+                current_group_count = 1;
+            }
+            Ordering::Greater => return false,
+        }
+    }
+    if current_group_count == 2 {
+        has_group_of_2_digits = true
+    }
+    has_group_of_2_digits
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,5 +84,12 @@ mod tests {
         assert_eq!(is_password_part1(get_digits(111111)), true);
         assert_eq!(is_password_part1(get_digits(223450)), false);
         assert_eq!(is_password_part1(get_digits(123789)), false);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(is_password_part2(get_digits(112233)), true);
+        assert_eq!(is_password_part2(get_digits(123444)), false);
+        assert_eq!(is_password_part2(get_digits(111122)), true);
     }
 }
