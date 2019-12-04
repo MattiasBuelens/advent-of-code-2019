cfe08c342481ea2beed443e2fd09c3f04e662423
Mattias Buelens <mattias@buelens.com>
2019-12-04 13:13:01 +0100

    Refactor and add tests

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,13 +1,19 @@
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Answer to part 1: {}", part1(372037, 905157));
+    let start = 372037;
+    let end = 905157;
+    println!("Answer to part 1: {}", part1(start, end));
 }
 
 fn part1(start: u32, end: u32) -> i32 {
+    solve(start, end, is_password_part1)
+}
+
+fn solve<F: Fn([u8; 6]) -> bool>(start: u32, end: u32, is_password: F) -> i32 {
     let mut total_passwords = 0;
     for pass in start..=end {
-        if is_password(pass) {
+        if is_password(get_digits(pass)) {
             total_passwords += 1
         }
     }
@@ -23,8 +29,7 @@ fn get_digits(mut number: u32) -> [u8; 6] {
     digits
 }
 
-fn is_password(pass: u32) -> bool {
-    let digits = get_digits(pass);
+fn is_password_part1(digits: [u8; 6]) -> bool {
     let mut has_same = false;
     for i in 1..digits.len() {
         match digits[i - 1].cmp(&digits[i]) {
@@ -40,4 +45,16 @@ fn is_password(pass: u32) -> bool {
         }
     }
     has_same
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(is_password_part1(get_digits(111111)), true);
+        assert_eq!(is_password_part1(get_digits(223450)), false);
+        assert_eq!(is_password_part1(get_digits(123789)), false);
+    }
 }
