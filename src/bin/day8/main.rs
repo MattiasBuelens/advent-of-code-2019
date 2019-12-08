29973c27b93fd540d5733e846f9b1b83702ea0ae
Mattias Buelens <mattias@buelens.com>
2019-12-08 10:40:29 +0100

    Refactor

--- src/bin/day8/main.rs
+++ src/bin/day8/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let input: Vec<u8> = parse_input(include_str!("input"));
+    let input: Vec<Color> = parse_input(include_str!("input"));
     let width = 25;
     let height = 6;
     println!("Answer to part 1: {}", part1(&input, width, height));
@@ -7,42 +7,72 @@ fn main() {
     print_image(&part2(&input, width, height));
 }
 
-fn parse_input(input: &str) -> Vec<u8> {
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum Color {
+    BLACK,
+    WHITE,
+    TRANSPARENT,
+}
+
+impl Color {
+    fn parse(digit: u32) -> Color {
+        match digit {
+            0 => Color::BLACK,
+            1 => Color::WHITE,
+            2 => Color::TRANSPARENT,
+            _ => panic!("invalid color {}", digit),
+        }
+    }
+
+    fn print(&self) -> char {
+        match *self {
+            Color::WHITE => '#',
+            _ => ' ',
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Color> {
     return input
         .trim()
         .chars()
-        .map(|x| x.to_digit(10).expect("invalid input") as u8)
+        .map(|x| x.to_digit(10).expect("invalid input"))
+        .map(Color::parse)
         .collect();
 }
 
-fn part1(input: &Vec<u8>, width: usize, height: usize) -> usize {
+fn part1(input: &Vec<Color>, width: usize, height: usize) -> usize {
     let layers = input.chunks_exact(width * height);
     let min_layer = layers
-        .min_by_key(|layer| layer.iter().filter(|x| **x == 0).count())
+        .min_by_key(|layer| layer.iter().filter(|x| **x == Color::BLACK).count())
         .expect("expected at least one layer");
-    let one_digits = min_layer.iter().filter(|x| **x == 1).count();
-    let two_digits = min_layer.iter().filter(|x| **x == 2).count();
+    let one_digits = min_layer.iter().filter(|x| **x == Color::WHITE).count();
+    let two_digits = min_layer
+        .iter()
+        .filter(|x| **x == Color::TRANSPARENT)
+        .count();
     one_digits * two_digits
 }
 
-fn part2(input: &Vec<u8>, width: usize, height: usize) -> Vec<Vec<char>> {
-    let mut output: Vec<Vec<char>> = Vec::with_capacity(height);
+fn part2(input: &Vec<Color>, width: usize, height: usize) -> Vec<Vec<Color>> {
+    let mut output: Vec<Vec<Color>> = Vec::with_capacity(height);
     for y in 0..height {
-        let mut row: Vec<char> = Vec::with_capacity(width);
+        let mut row: Vec<Color> = Vec::with_capacity(width);
         for x in 0..width {
-            let layer = input
+            let color = input
                 .chunks_exact(width * height)
-                .find(|layer| layer[y * width + x] != 2)
+                .map(|layer| layer[y * width + x])
+                .find(|color| *color != Color::TRANSPARENT)
                 .expect(&format!("missing color at {},{}", x, y));
-            row.push(if layer[y * width + x] == 1 { '#' } else { ' ' });
+            row.push(color);
         }
         output.push(row);
     }
     output
 }
 
-fn print_image(image: &Vec<Vec<char>>) {
+fn print_image(image: &Vec<Vec<Color>>) {
     for line in image {
-        println!("{}", line.iter().collect::<String>())
+        println!("{}", line.iter().map(Color::print).collect::<String>())
     }
 }
