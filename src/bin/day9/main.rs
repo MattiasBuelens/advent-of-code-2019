16691606fe4046283facc999892eee38268b5ea9
Mattias Buelens <mattias@buelens.com>
2019-12-09 22:56:35 +0100

    Add test for day 9

--- src/bin/day9/main.rs
+++ src/bin/day9/main.rs
@@ -20,3 +20,33 @@ fn part2(program: &Vec<i64>) -> i64 {
     assert_eq!(output.len(), 1);
     output[0]
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quine() {
+        let program = vec![
+            109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99,
+        ];
+        let output = Machine::new(program.clone(), vec![]).run();
+        assert_eq!(output, program);
+    }
+
+    #[test]
+    fn test_16_digit() {
+        let program = vec![1102, 34915192, 34915192, 7, 4, 7, 99, 0];
+        let output = Machine::new(program, vec![]).run();
+        assert_eq!(output.len(), 1);
+        assert_eq!(output[0].to_string().len(), 16);
+    }
+
+    #[test]
+    fn test_large_number() {
+        let large_number = 1125899906842624;
+        let program = vec![104, large_number, 99];
+        let output = Machine::new(program, vec![]).run();
+        assert_eq!(output, vec![large_number]);
+    }
+}
