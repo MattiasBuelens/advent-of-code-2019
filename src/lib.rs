2ed8673e4ec3cfcb31d487dd212b448e26c95139
Mattias Buelens <mattias@buelens.com>
2019-12-12 08:41:22 +0100

    Add Vector3D

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod input;
 pub mod intcode;
 pub mod vector2d;
+pub mod vector3d;
---
+++ src/vector3d.rs
@@ -0,0 +1,64 @@
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
+pub struct Vector3D {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+impl Vector3D {
+    pub fn new(x: i32, y: i32, z: i32) -> Vector3D {
+        Vector3D { x, y, z }
+    }
+
+    pub fn zero() -> Vector3D {
+        Vector3D::new(0, 0, 0)
+    }
+}
+
+impl Default for Vector3D {
+    fn default() -> Self {
+        Vector3D::zero()
+    }
+}
+
+impl Add for Vector3D {
+    type Output = Self;
+
+    fn add(self: Vector3D, other: Vector3D) -> Vector3D {
+        Vector3D {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for Vector3D {
+    type Output = Self;
+
+    fn sub(self: Vector3D, other: Vector3D) -> Vector3D {
+        Vector3D {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl AddAssign for Vector3D {
+    fn add_assign(&mut self, other: Self) {
+        self.x.add_assign(other.x);
+        self.y.add_assign(other.y);
+        self.z.add_assign(other.z);
+    }
+}
+
+impl SubAssign for Vector3D {
+    fn sub_assign(&mut self, other: Self) {
+        self.x.sub_assign(other.x);
+        self.y.sub_assign(other.y);
+        self.z.sub_assign(other.z);
+    }
+}
