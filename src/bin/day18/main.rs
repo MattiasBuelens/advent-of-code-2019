dda16300020ae60167b36dc2dda1511abd466c58
Mattias Buelens <mattias@buelens.com>
2019-12-18 13:36:16 +0100

    Solve part 1 examples 1 to 3

--- src/bin/day18/main.rs
+++ src/bin/day18/main.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+use pathfinding::directed::dijkstra::dijkstra_all;
 
 use advent_of_code_2019::vector2d::Vector2D;
 
@@ -80,9 +82,64 @@ fn print_grid(grid: &Grid, you: &Vector2D) {
 }
 
 fn part1(grid: &Grid, start: &Vector2D) -> i32 {
-    print_grid(grid, start);
+    // print_grid(grid, start);
+    let owned_keys: HashSet<char> = HashSet::new();
+    let result = get_least_steps(grid, start, &owned_keys);
+    result
+}
 
-    0
+fn get_least_steps(grid: &Grid, start: &Vector2D, owned_keys: &HashSet<char>) -> i32 {
+    let distances = get_distances_from(grid, start, &owned_keys);
+    let mut best: Option<i32> = None;
+    for (pos, (_, distance)) in distances.iter() {
+        if let Some(Tile::Key(letter)) = grid.get(pos) {
+            if !owned_keys.contains(letter) {
+                // Found a key that we don't have yet.
+                // Pick it up and continue from here.
+                let mut owned_keys = owned_keys.clone();
+                owned_keys.insert(*letter);
+                let result = *distance + get_least_steps(grid, pos, &owned_keys);
+                best = Some(best.map_or(result, |best| best.min(result)));
+            }
+        }
+    }
+    // If we already own all keys, then `best` will be `None` and we don't need to go anywhere else.
+    best.unwrap_or(0)
+}
+
+fn get_distances_from(
+    grid: &Grid,
+    start: &Vector2D,
+    owned_keys: &HashSet<char>,
+) -> HashMap<Vector2D, (Vector2D, i32)> {
+    dijkstra_all(start, |pos| {
+        get_neighbours(*pos)
+            .iter()
+            .filter(|neighbour| {
+                grid.get(neighbour)
+                    .map_or(false, |tile| can_traverse(tile, owned_keys))
+            })
+            .map(|neighbour| (*neighbour, 1))
+            .collect::<Vec<_>>()
+    })
+}
+
+fn get_neighbours(position: Vector2D) -> Vec<Vector2D> {
+    vec![
+        position + Vector2D::new(0, 1),
+        position + Vector2D::new(0, -1),
+        position + Vector2D::new(1, 0),
+        position + Vector2D::new(-1, 0),
+    ]
+}
+
+fn can_traverse(tile: &Tile, owned_keys: &HashSet<char>) -> bool {
+    match *tile {
+        Tile::Open => true,
+        Tile::Wall => false,
+        Tile::Key(_) => true,
+        Tile::Door(letter) => owned_keys.contains(&letter.to_ascii_lowercase()),
+    }
 }
 
 fn part2(grid: &Grid, start: &Vector2D) -> i32 {
@@ -94,5 +151,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1_example1() {}
+    fn test_part1_example1() {
+        let (grid, start) = parse_grid(include_str!("example1"));
+        assert_eq!(part1(&grid, &start), 8);
+    }
+
+    #[test]
+    fn test_part1_example2() {
+        let (grid, start) = parse_grid(include_str!("example2"));
+        assert_eq!(part1(&grid, &start), 86);
+    }
+
+    #[test]
+    fn test_part1_example3() {
+        let (grid, start) = parse_grid(include_str!("example3"));
+        assert_eq!(part1(&grid, &start), 132);
+    }
 }
