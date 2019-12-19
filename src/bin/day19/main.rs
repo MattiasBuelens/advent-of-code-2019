fd0bc8f80c5fb637a6c595fa7a6922af3ea50f44
Mattias Buelens <mattias@buelens.com>
2019-12-19 09:23:52 +0100

    Solve day 19 part 2

--- src/bin/day19/main.rs
+++ src/bin/day19/main.rs
@@ -22,7 +22,6 @@ fn part1(program: &Vec<i64>) -> usize {
             }
         }
     }
-    // print_beam(&beam);
     beam.len()
 }
 
@@ -38,6 +37,9 @@ fn in_beam(program: &Vec<i64>, pos: &Vector2D) -> bool {
 }
 
 fn print_beam(beam: &Beam) {
+    if beam.is_empty() {
+        return;
+    }
     let min_x = beam.iter().min_by_key(|pos| pos.x).unwrap().x;
     let min_y = beam.iter().min_by_key(|pos| pos.y).unwrap().y;
     let max_x = beam.iter().max_by_key(|pos| pos.x).unwrap().x;
@@ -57,7 +59,62 @@ fn print_beam(beam: &Beam) {
 }
 
 fn part2(program: &Vec<i64>) -> i32 {
-    0
+    let size = 100;
+    let mut beam: Beam = HashSet::new();
+    let mut min_x = 0;
+    let mut min_y = 0;
+    let mut corner_br = None;
+    'outer: for limit in 1.. {
+        for x in min_x..=limit {
+            let pos = Vector2D::new(x, limit);
+            if in_beam(&program, &pos) {
+                break;
+            } else {
+                min_x += 1;
+            }
+        }
+        for x in min_x..=limit {
+            let pos = Vector2D::new(x, limit);
+            if in_beam(&program, &pos) {
+                beam.insert(pos);
+                if fits_square(&beam, size, &pos) {
+                    corner_br = Some(pos);
+                    break 'outer;
+                }
+            }
+        }
+        for y in min_y..limit {
+            let pos = Vector2D::new(limit, y);
+            if in_beam(&program, &pos) {
+                break;
+            } else {
+                min_y += 1;
+            }
+        }
+        for y in min_y..limit {
+            let pos = Vector2D::new(limit, y);
+            if in_beam(&program, &pos) {
+                beam.insert(pos);
+                if fits_square(&beam, size, &pos) {
+                    corner_br = Some(pos);
+                    break 'outer;
+                }
+            }
+        }
+    }
+    // print_beam(&beam);
+
+    let corner_br = corner_br.unwrap();
+    let corner_tl = corner_br - Vector2D::new(size - 1, size - 1);
+
+    corner_tl.x * 10_000 + corner_tl.y
+}
+
+fn fits_square(beam: &Beam, size: i32, corner: &Vector2D) -> bool {
+    beam.contains(corner)
+        && beam.contains(&(*corner - Vector2D::new(0, size - 1)))
+        && beam.contains(&(*corner - Vector2D::new(size - 1, 0)))
+        && beam.contains(&(*corner - Vector2D::new(size - 1, size - 1)))
 }
 
 #[cfg(test)]
