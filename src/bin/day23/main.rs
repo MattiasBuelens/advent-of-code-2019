19865ee2bd886adab6528d5c047ed1ff5804a1d0
Mattias Buelens <mattias@buelens.com>
2019-12-23 10:09:10 +0100

    Solve day 23 part 2

--- src/bin/day23/main.rs
+++ src/bin/day23/main.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("Answer to part 2: {}", part2(&program));
 }
 
+#[derive(Debug)]
 struct Message {
     address: i64,
     x: i64,
@@ -18,6 +19,7 @@ struct Computer {
     address: i64,
     machine: ProgramMachine,
     queue: VecDeque<Message>,
+    waiting_for_input: bool,
 }
 
 impl Computer {
@@ -26,6 +28,7 @@ impl Computer {
             address,
             machine: ProgramMachine::new(program, vec![address]),
             queue: VecDeque::new(),
+            waiting_for_input: false,
         }
     }
 
@@ -34,14 +37,17 @@ impl Computer {
             StepResult::Ok => None,
             StepResult::NeedInput => {
                 if let Some(message) = self.queue.pop_front() {
+                    self.waiting_for_input = false;
                     self.machine.add_input(message.x);
                     self.machine.add_input(message.y);
                 } else {
+                    self.waiting_for_input = true;
                     self.machine.add_input(-1);
                 }
                 None
             }
             StepResult::Output(address) => {
+                self.waiting_for_input = false;
                 let x = self.machine.run_to_output().unwrap();
                 let y = self.machine.run_to_output().unwrap();
                 Some(Message { address, x, y })
@@ -51,8 +57,11 @@ impl Computer {
     }
 
     fn send(&mut self, message: Message) {
-        assert_eq!(self.address, message.address);
         self.queue.push_back(message);
+    }
+
+    fn is_idle(&self) -> bool {
+        self.waiting_for_input && self.queue.is_empty()
     }
 }
 
@@ -74,5 +83,32 @@ fn part1(program: &Vec<i64>) -> i64 {
 }
 
 fn part2(program: &Vec<i64>) -> i64 {
-    0
+    let mut computers = (0..50)
+        .map(|address| Computer::new(address, program.clone()))
+        .collect::<Vec<_>>();
+    let mut nat_message: Option<Message> = None;
+    let mut last_nat_y: Option<i64> = None;
+    loop {
+        for i in 0..computers.len() {
+            if let Some(message) = computers[i].step() {
+                if message.address == 255 {
+                    nat_message = Some(message);
+                } else {
+                    computers[message.address as usize].send(message);
+                }
+            }
+        }
+        let all_idle = computers.iter().all(|x| x.is_idle());
+        if all_idle {
+            if let Some(message) = nat_message.take() {
+                if let Some(last_nat_y) = last_nat_y {
+                    if last_nat_y == message.y {
+                        return message.y;
+                    }
+                }
+                last_nat_y = Some(message.y);
+                computers[0].send(message);
+            }
+        }
+    }
 }
