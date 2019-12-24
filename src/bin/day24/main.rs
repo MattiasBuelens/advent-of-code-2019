6a9978fc01c03d1693b443dffef24f0c596a068a
Mattias Buelens <mattias@buelens.com>
2019-12-24 11:22:32 +0100

    Reformat

--- src/bin/day24/main.rs
+++ src/bin/day24/main.rs
@@ -43,79 +43,79 @@ struct Grid {
 }
 
 impl Grid {
-fn parse(input: &str) -> Self {
-    let mut tiles: [[Tile; SIZE]; SIZE] = Default::default();
-    let mut y = 0;
-    for line in input.trim().lines() {
-        let mut x = 0;
-        for value in line.chars() {
-            tiles[y][x] = Tile::parse(value);
-            x += 1;
+    fn parse(input: &str) -> Self {
+        let mut tiles: [[Tile; SIZE]; SIZE] = Default::default();
+        let mut y = 0;
+        for line in input.trim().lines() {
+            let mut x = 0;
+            for value in line.chars() {
+                tiles[y][x] = Tile::parse(value);
+                x += 1;
+            }
+            y += 1;
         }
-        y += 1;
+        Grid { tiles }
     }
-    Grid { tiles }
-}
 
-fn print(&self) {
-    for row in &self.tiles {
-        println!("{}", row.iter().map(|x| x.print()).collect::<String>());
+    fn print(&self) {
+        for row in &self.tiles {
+            println!("{}", row.iter().map(|x| x.print()).collect::<String>());
+        }
     }
-}
 
-fn get_neighbours(&self, x: usize, y: usize) -> Vec<Tile> {
-    let mut neighbours: Vec<Tile> = Vec::new();
-    if y > 0 {
-        neighbours.push(self.tiles[y - 1][x]);
+    fn get_neighbours(&self, x: usize, y: usize) -> Vec<Tile> {
+        let mut neighbours: Vec<Tile> = Vec::new();
+        if y > 0 {
+            neighbours.push(self.tiles[y - 1][x]);
+        }
+        if x > 0 {
+            neighbours.push(self.tiles[y][x - 1]);
+        }
+        if x + 1 < SIZE {
+            neighbours.push(self.tiles[y][x + 1]);
+        }
+        if y + 1 < SIZE {
+            neighbours.push(self.tiles[y + 1][x]);
+        }
+        neighbours
     }
-    if x > 0 {
-        neighbours.push(self.tiles[y][x - 1]);
-    }
-    if x + 1 < SIZE {
-        neighbours.push(self.tiles[y][x + 1]);
-    }
-    if y + 1 < SIZE {
-        neighbours.push(self.tiles[y + 1][x]);
-    }
-    neighbours
-}
 
-fn step(&mut self) {
-    let old_grid = self.clone();
-    for y in 0..SIZE {
-        for x in 0..SIZE {
-            let neighbour_bugs = old_grid
-                .get_neighbours(x, y)
-                .iter()
-                .filter(|&x| x == &Tile::BUG)
-                .count();
-            self.tiles[y][x] = match (old_grid.tiles[y][x], neighbour_bugs) {
-                // A bug dies (becoming an empty space) unless there is exactly one bug
-                // adjacent to it.
-                (Tile::BUG, 1) => Tile::BUG,
-                (Tile::BUG, _) => Tile::EMPTY,
-                // An empty space becomes infested with a bug if exactly one or two bugs
-                // are adjacent to it.
-                (Tile::EMPTY, 1) | (Tile::EMPTY, 2) => Tile::BUG,
-                (Tile::EMPTY, _) => Tile::EMPTY,
+    fn step(&mut self) {
+        let old_grid = self.clone();
+        for y in 0..SIZE {
+            for x in 0..SIZE {
+                let neighbour_bugs = old_grid
+                    .get_neighbours(x, y)
+                    .iter()
+                    .filter(|&x| x == &Tile::BUG)
+                    .count();
+                self.tiles[y][x] = match (old_grid.tiles[y][x], neighbour_bugs) {
+                    // A bug dies (becoming an empty space) unless there is exactly one bug
+                    // adjacent to it.
+                    (Tile::BUG, 1) => Tile::BUG,
+                    (Tile::BUG, _) => Tile::EMPTY,
+                    // An empty space becomes infested with a bug if exactly one or two bugs
+                    // are adjacent to it.
+                    (Tile::EMPTY, 1) | (Tile::EMPTY, 2) => Tile::BUG,
+                    (Tile::EMPTY, _) => Tile::EMPTY,
+                }
             }
         }
     }
-}
 
-fn get_biodiversity_rating(&self) -> u32 {
-    let mut rating = 0;
-    let mut power = 1;
-    for y in 0..SIZE {
-        for x in 0..SIZE {
-            if self.tiles[y][x] == Tile::BUG {
-                rating |= power;
+    fn get_biodiversity_rating(&self) -> u32 {
+        let mut rating = 0;
+        let mut power = 1;
+        for y in 0..SIZE {
+            for x in 0..SIZE {
+                if self.tiles[y][x] == Tile::BUG {
+                    rating |= power;
+                }
+                power <<= 1;
             }
-            power <<= 1;
         }
+        rating
     }
-    rating
-}
 }
 
 fn part1(mut grid: Grid) -> u32 {
