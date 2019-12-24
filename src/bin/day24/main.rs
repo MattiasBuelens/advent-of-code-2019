c0b0ae19d090cd0d8a6284192fab1f74b7c90126
Mattias Buelens <mattias@buelens.com>
2019-12-24 11:22:11 +0100

    Refactor

--- src/bin/day24/main.rs
+++ src/bin/day24/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 fn main() {
-    let grid: Grid = parse_input(include_str!("input"));
+    let grid: Grid = Grid::parse(include_str!("input"));
     println!("Answer to part 1: {}", part1(grid.clone()));
     println!("Answer to part 2: {}", part2(grid.clone()));
 }
@@ -37,54 +37,59 @@ impl Default for Tile {
 
 const SIZE: usize = 5;
 
-type Grid = [[Tile; SIZE]; SIZE];
+#[derive(Debug, Copy, Clone)]
+struct Grid {
+    tiles: [[Tile; SIZE]; SIZE],
+}
 
-fn parse_input(input: &str) -> Grid {
-    let mut grid: Grid = Default::default();
+impl Grid {
+fn parse(input: &str) -> Self {
+    let mut tiles: [[Tile; SIZE]; SIZE] = Default::default();
     let mut y = 0;
     for line in input.trim().lines() {
         let mut x = 0;
         for value in line.chars() {
-            grid[y][x] = Tile::parse(value);
+            tiles[y][x] = Tile::parse(value);
             x += 1;
         }
         y += 1;
     }
-    grid
+    Grid { tiles }
 }
 
-fn print_grid(grid: &Grid) {
-    for row in grid {
+fn print(&self) {
+    for row in &self.tiles {
         println!("{}", row.iter().map(|x| x.print()).collect::<String>());
     }
 }
 
-fn get_neighbours(grid: &Grid, x: usize, y: usize) -> Vec<Tile> {
+fn get_neighbours(&self, x: usize, y: usize) -> Vec<Tile> {
     let mut neighbours: Vec<Tile> = Vec::new();
     if y > 0 {
-        neighbours.push(grid[y - 1][x]);
+        neighbours.push(self.tiles[y - 1][x]);
     }
     if x > 0 {
-        neighbours.push(grid[y][x - 1]);
+        neighbours.push(self.tiles[y][x - 1]);
     }
     if x + 1 < SIZE {
-        neighbours.push(grid[y][x + 1]);
+        neighbours.push(self.tiles[y][x + 1]);
     }
     if y + 1 < SIZE {
-        neighbours.push(grid[y + 1][x]);
+        neighbours.push(self.tiles[y + 1][x]);
     }
     neighbours
 }
 
-fn step(grid: Grid) -> Grid {
-    let mut new_grid: Grid = Default::default();
+fn step(&mut self) {
+    let old_grid = self.clone();
     for y in 0..SIZE {
         for x in 0..SIZE {
-            let neighbour_bugs = get_neighbours(&grid, x, y)
+            let neighbour_bugs = old_grid
+                .get_neighbours(x, y)
                 .iter()
                 .filter(|&x| x == &Tile::BUG)
                 .count();
-            new_grid[y][x] = match (grid[y][x], neighbour_bugs) {
+            self.tiles[y][x] = match (old_grid.tiles[y][x], neighbour_bugs) {
                 // A bug dies (becoming an empty space) unless there is exactly one bug
                 // adjacent to it.
                 (Tile::BUG, 1) => Tile::BUG,
@@ -96,15 +101,14 @@ fn step(grid: Grid) -> Grid {
             }
         }
     }
-    new_grid
 }
 
-fn get_biodiversity_rating(grid: &Grid) -> u32 {
+fn get_biodiversity_rating(&self) -> u32 {
     let mut rating = 0;
     let mut power = 1;
     for y in 0..SIZE {
         for x in 0..SIZE {
-            if grid[y][x] == Tile::BUG {
+            if self.tiles[y][x] == Tile::BUG {
                 rating |= power;
             }
             power <<= 1;
@@ -112,14 +116,15 @@ fn get_biodiversity_rating(grid: &Grid) -> u32 {
     }
     rating
 }
+}
 
 fn part1(mut grid: Grid) -> u32 {
     let mut seen_ratings: HashSet<u32> = HashSet::new();
-    seen_ratings.insert(get_biodiversity_rating(&grid));
+    seen_ratings.insert(grid.get_biodiversity_rating());
 
     loop {
-        grid = step(grid);
-        let rating = get_biodiversity_rating(&grid);
+        grid.step();
+        let rating = grid.get_biodiversity_rating();
         if seen_ratings.contains(&rating) {
             break;
         } else {
@@ -127,8 +132,8 @@ fn part1(mut grid: Grid) -> u32 {
         }
     }
 
-    // print_grid(&grid);
-    get_biodiversity_rating(&grid)
+    // grid.print();
+    grid.get_biodiversity_rating()
 }
 
 fn part2(grid: Grid) -> u32 {
